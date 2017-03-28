9174d05120025999a9997e6c123529f61a6c8ce4
lapple06 <291608411@qq.com>
2017-03-28 19:53:23 +0800

    修改了注释

--- src/com/gdufe/Addr/Address.java
+++ src/com/gdufe/Addr/Address.java
@@ -5,4 +5,9 @@ public class Address {
 	public static final String SCHEDULEQUERY = "http://jwxt.gdufe.edu.cn/jsxsd/xskb/xskb_list.do";
 	public static final String SOCREQUERY = "http://jwxt.gdufe.edu.cn/jsxsd/kscj/cjcx_list";
 	public static final String STUDENTSTATUS = "http://jwxt.gdufe.edu.cn/jsxsd/grxx/xsxx?Ves632DSdyV=NEW_XSD_XJCJ";
+
+	public static final String PORTALLOGIN = "http://my.gdufe.edu.cn/userPasswordValidate.portal";
+	public static final String VERIFICATIONIMG = "http://my.gdufe.edu.cn/captchaGenerate.portal";
+	
+	
 }
